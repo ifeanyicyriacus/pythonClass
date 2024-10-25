51b0e045d605d14ba7a90862de2519608c7a74cd
ifeanyicyriacus <cyriacusifeanyi@gmail.com>
2024-10-25 14:12:25 +0100

    Completed assignment

---
+++ assignment/asa/python_specification.py
@@ -0,0 +1,9 @@
+import sys
+
+print(f"Python version: {sys.version}\n")
+print(f"Python path: {sys.path}\n")
+print(f"Python api_version: {sys.api_version}\n")
+print(f"Python copyright: {sys.copyright}\n")
+
+
+
