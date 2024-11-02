c0cabcb748ea6f372b57d9724f00472c8f5f820f
ifeanyicyriacus <cyriacusifeanyi@gmail.com>
2024-11-02 18:00:06 +0100

    progress at 6pm

---
+++ chapter_3/exercise_17.py
@@ -0,0 +1,24 @@
+#exercise 3.17 (Nexted Loops)
+counter_a = 1
+counter_b = 1
+
+print("(a)\t(b)\t(c)\t(d)")
+while(counter_b <=10):
+    counter_a = 1
+    while(counter_a <= 4):
+        if counter_a <= 1:
+            print(f"{"* " * counter_b}",end = "")
+            print(f"{"  " * (10 - counter_b)}",end = "")
+        elif counter_a <= 2:
+            print(f"{"* " * (11 - counter_b)}",end = "")
+            print(f"{"  " * (counter_b - 1)}",end = "")
+        elif counter_a <= 3:
+            print(f"{"  " * (10 - counter_b)}",end = "") 
+            print(f"{" *" * counter_b}",end = "")
+        else:
+            print(f"{"  " * (counter_b - 1)}",end = "")
+            print(f"{" *" * (11 - counter_b)}",end = "")
+        print("\t", end = "")
+        counter_a += 1
+    print()
+    counter_b += 1
