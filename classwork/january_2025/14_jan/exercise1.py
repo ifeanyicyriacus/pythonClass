2bf55e15baf1869f5b18856c430640ae2e4e6b13
ifeanyicyriacus <cyriacusifeanyi@gmail.com>
2025-01-14 15:01:37 +0100

    class task done

--- classwork/january_2025/14_jan/exercise1.py
+++ classwork/january_2025/14_jan/exercise1.py
@@ -1,13 +1,11 @@
 import math
 
 def parse_even_digit(text:str) -> list:
-    result = []
-    for element in text:
-        if element.isdigit():
-            if int(element) % 2 == 0:
-                result.append(int(element))
-    return result
+    return [x for x in list([int(x) for x in text if x.isdigit()]) if x % 2 == 0]
 
 def number_to_square_dict(number:int) -> dict:
     dictionary = {1: number, 2: math.pow(number, 2)}
     return dictionary
+
+def filter_between_20_and_50(numbers:list) -> list:
+    return list(filter(lambda number: 20 <= number <= 50, numbers))
--- classwork/january_2025/14_jan/test_exercise1.py
+++ classwork/january_2025/14_jan/test_exercise1.py
@@ -6,13 +6,22 @@ class TestParseEvenDigitFunction(TestCase):
         self.assertEqual(parse_even_digit(""), [])
 
     def test_that_parse_even_digit_returns_correct_value(self):
-        actual = parse_even_digit("73HAdSdF39dm")
-        expected = []
+        actual = parse_even_digit("73H2AdS8d9F39dm")
+        expected = [2, 8]
         self.assertEqual(actual, expected)
 
 class TestNumberSquareFunction(TestCase):
     def test_that_number_to_square_dict_return_correct_value(self):
         self.assertEqual(number_to_square_dict(9), {1:9, 2:81})
+
+class TestFilterBetween20And50Function(TestCase):
+    def test_that_filter_between_20_and_50_exist(self):
+        self.assertEqual(filter_between_20_and_50([]), [])
+
+    def test_that_filter_between_50_and_20_return_correct_value(self):
+        actual = filter_between_20_and_50([12, 15, 19, 21, 50, 70])
+        expected = [21, 50]
+        self.assertEqual(actual, expected)
 
 # if __name__ == '__main__':
 #     unittest.main()
