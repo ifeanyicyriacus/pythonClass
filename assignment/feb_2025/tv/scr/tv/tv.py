fa8b49e5688d4df57cb2b389a107fe36f49f9038
ifeanyicyriacus <cyriacusifeanyi@gmail.com>
2025-02-08 03:08:30 +0100

    Completed  - test_tv.py  - tv.py

--- assignment/feb_2025/tv/scr/tv/tv.py
+++ assignment/feb_2025/tv/scr/tv/tv.py
@@ -1,51 +1,88 @@
+
 class StateException(Exception):
     def __init__(self, msg):
         super(StateException, self).__init__(msg)
 
 
 class TV:
+    MAX_VOLUME: int = 100
+    MIN_VOLUME: int = 0
+    MAX_CHANNEL: int = 100
+    MIN_CHANNEL: int = 0
+
     def __init__(self):
-        self._is_on = False
-        self._volume_level = 0
-        self._channel_level = 0
+        self.__tempVolume:int = 0
+        self.__is_on = False
+        self.__volume_level = self.MIN_VOLUME
+        self.__channel_level = self.MIN_CHANNEL
 
     @property
     def is_on(self):
-        return self._is_on
+        return self.__is_on
+
     @is_on.setter
-    def is_on(self, value:bool) -> None:
-        self._is_on = value
+    def is_on(self, value: bool) -> None:
+        self.__is_on = value
 
     @property
     def volume_level(self):
-        return self._volume_level
+        return self.__volume_level
+
     @volume_level.setter
-    def volume_level(self, value:int) -> None:
-        self._volume_level = value
+    def volume_level(self, value: int) -> None:
+        self.__volume_level = value
 
     @property
     def channel_level(self):
-        return self._channel_level
+        return self.__channel_level
 
     @channel_level.setter
-    def channel_level(self, value:int) -> None:
-        self._channel_level = value
+    def channel_level(self, value: int) -> None:
+        self.__channel_level = value
 
     def turn_on(self):
-        self._is_on = True
+        self.is_on = True
 
     def turn_off(self):
-        self._is_on = False
+        self.is_on = False
 
     def increase_volume(self):
-        if not self._is_on:
-            raise StateException('TV is not turned on')
-        # raise warning sound alert
-        if self.volume_level < 100:
+        self.__power_on_check()
+        if self.volume_level < self.MAX_VOLUME:
             self.volume_level += 1
 
     def decrease_volume(self):
-        if not self._is_on:
-            raise StateException('TV is not turned on')
-        if self.volume_level > 0:
+        self.__power_on_check()
+        if self.volume_level > self.MIN_VOLUME:
             self.volume_level -= 1
+
+    def channel_up(self):
+        self.__power_on_check()
+        if self.channel_level < self.MAX_CHANNEL:
+            self.channel_level += 1
+
+    def channel_down(self):
+        self.__power_on_check()
+        if self.channel_level > self.MIN_CHANNEL:
+            self.channel_level -= 1
+
+    def set_channel(self, channel_level):
+        self.__power_on_check()
+        if self.MIN_CHANNEL <= channel_level <= self.MAX_CHANNEL:
+            self.channel_level = channel_level
+            return
+        raise ValueError('invalid channel level Entered')
+
+    def mute(self) -> None:
+        self.__power_on_check()
+        self.__tempVolume = self.volume_level
+        self.volume_level = self.MIN_VOLUME
+
+    def unmute(self):
+        self.__power_on_check()
+        self.volume_level = self.__tempVolume
+
+    def __power_on_check(self):
+        if not self.is_on:
+            raise StateException('TV is not turned on')
+
--- assignment/feb_2025/tv/test/tv/test_tv.py
+++ assignment/feb_2025/tv/test/tv/test_tv.py
@@ -4,6 +4,13 @@ from tv.tv import TV, StateException
 
 
 class MyTestCase(TestCase):
+    MAX_VOLUME: int = 100
+    MIN_VOLUME: int = 0
+    MAX_CHANNEL: int = 100
+    MIN_CHANNEL: int = 0
+    BELOW_VOLUME_THRESHOLD:int = -1
+    ABOVE_VOLUME_THRESHOLD:int = 101
+
     def setUp(self):
         self.tv = TV()
 
@@ -54,4 +61,88 @@ class MyTestCase(TestCase):
         self.tv.decrease_volume()
         self.assertEqual(7, self.tv.volume_level)
 
+    def test_tv_volume_can_not_decreased_less_than_0_percent(self):
+        self.tv.turn_on()
+        self.assertEqual(0, self.tv.volume_level)
+        for i in range(10):
+            self.tv.increase_volume()
+        self.assertEqual(10, self.tv.volume_level)
+
+        for i in range(50):
+            self.tv.decrease_volume()
+        self.assertEqual(0, self.tv.volume_level)
+
+    def test_tv_channel_can_be_increased_only_when_turned_on(self):
+        self.assertFalse(self.tv.is_on)
+        self.assertRaises(StateException, self.tv.channel_up)
+        self.tv.turn_on()
+        self.assertTrue(self.tv.is_on)
+        self.assertEqual(0, self.tv.channel_level)
+        self.tv.channel_up()
+        self.assertEqual(1, self.tv.channel_level)
+        self.tv.channel_up()
+        self.assertEqual(2, self.tv.channel_level)
+
+    def test_tv_channel_can_be_decreased_only_when_turned_on(self):
+        self.assertFalse(self.tv.is_on)
+        self.assertRaises(StateException, self.tv.channel_down)
+        self.tv.turn_on()
+        self.assertTrue(self.tv.is_on)
+        self.assertEqual(0, self.tv.channel_level)
+        for i in range(10):
+            self.tv.channel_up()
+        self.assertEqual(10, self.tv.channel_level)
+        for i in range(9):
+            self.tv.channel_down()
+        self.assertEqual(1, self.tv.channel_level)
+        self.tv.channel_down()
+        self.assertEqual(0, self.tv.channel_level)
+
+    def test_tv_channel_can_not_be_increased_above_100_percent(self):
+        self.tv.turn_on()
+        self.assertEqual(0, self.tv.channel_level)
+        for i in range(150):
+            self.tv.channel_up()
+        self.assertEqual(100, self.tv.channel_level)
+
+    def test_tv_channel_can_not_be_decreased_below_0_percent(self):
+        self.tv.turn_on()
+        self.assertEqual(0, self.tv.channel_level)
+        for i in range(50):
+            self.tv.channel_up()
+        self.assertEqual(50, self.tv.channel_level)
+        for i in range(100):
+            self.tv.channel_down()
+        self.assertEqual(0, self.tv.channel_level)
+
+    def test_tv_channel_can_be_set_to_a_specified_number(self):
+        self.tv.turn_on()
+        self.assertEqual(0, self.tv.channel_level)
+        channel_number = 50
+        self.tv.set_channel(channel_number)
+        self.assertEqual(50, self.tv.channel_level)
+
+    def test_tv_channel_can_only_be_set_if_within_range_0_and_100_percent(self):
+        self.tv.turn_on()
+        self.tv.set_channel(23)
+        self.assertRaises(ValueError, self.tv.set_channel, self.BELOW_VOLUME_THRESHOLD)
+        self.assertRaises(ValueError, self.tv.set_channel, self.ABOVE_VOLUME_THRESHOLD)
+
+    def test_tv_sound_can_be_muted(self):
+        self.tv.turn_on()
+        for i in range(50):
+            self.tv.increase_volume()
+        self.assertEqual(50, self.tv.volume_level)
+        self.tv.mute()
+        self.assertEqual(self.MIN_VOLUME, self.tv.volume_level)
+
+    def test_tv_sound_can_be_unmuted(self):
+        self.tv.turn_on()
+        for i in range(50):
+            self.tv.increase_volume()
+        self.assertEqual(50, self.tv.volume_level)
+        self.tv.mute()
+        self.assertEqual(self.MIN_VOLUME, self.tv.volume_level)
+        self.tv.unmute()
+        self.assertEqual(50, self.tv.volume_level)
 
