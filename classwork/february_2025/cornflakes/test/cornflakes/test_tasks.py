5b667d886ff7f57e225342bb02b78823378352a6
ifeanyicyriacus <cyriacusifeanyi@gmail.com>
2025-02-27 10:37:19 +0100

    initial commit

---
+++ classwork/february_2025/cornflakes/test/cornflakes/test_tasks.py
@@ -0,0 +1,10 @@
+import unittest
+from src.cornflakes.task1 import *
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, False)  # add assertion here
+
+
+if __name__ == '__main__':
+    unittest.main()
