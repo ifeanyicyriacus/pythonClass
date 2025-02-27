90d4ddfa2f200da68e1f8c18a62cc4fc9a3fb2ef
ifeanyicyriacus <cyriacusifeanyi@gmail.com>
2025-02-27 22:41:27 +0100

    completed 7 segment display completed: validating input completed: add possibly a user interface

---
+++ assignment/feb_2025/seven_segment_display/src/main.py
@@ -0,0 +1,29 @@
+import sys
+
+from src.seven_segment_display.seven_segment_display import SevenSegmentDisplay
+
+
+class Main:
+    display:SevenSegmentDisplay = SevenSegmentDisplay()
+
+    menu_prompt = """
+    
+    ------SEVEN SEGMENT DISPLAY----
+    1. Prompt Segment Display (8-digit binary)
+    0. Exit
+    
+    """
+
+    def start():
+        while True:
+            user_input:str = input(Main.menu_prompt)
+            if user_input == "0": sys.exit(0)
+            if user_input == "1":
+                user_input = input("Enter 8-digit binary to continue...:")
+                try:
+                    Main.display.enter_prompt(user_input)
+                    print(Main.display)
+                except ValueError as e:
+                    print(e)
+
+Main.start()
