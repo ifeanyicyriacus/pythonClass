4e9e56399fde160b5af70e03949157f057eb80e9
ifeanyicyriacus <cyriacusifeanyi@gmail.com>
2025-02-27 19:11:44 +0100

    completed 7 segment display working on validating input and possibly a user interface

--- assignment/feb_2025/seven_segment_display/src/seven_segment_display/seven_segment_display.py
+++ assignment/feb_2025/seven_segment_display/src/seven_segment_display/seven_segment_display.py
@@ -1,3 +1,6 @@
+
+
+
 class SevenSegmentDisplay:
 
     def __init__(self):
@@ -17,18 +20,77 @@ class SevenSegmentDisplay:
     def set_segments_value(self, prompt:str):
         prompt_char = [x for x in prompt]
         self.__set_display_blank()
-        self.__set_a(prompt_char[0] == "1")
-        self.__set_a(prompt_char[1] == "1")
-        self.__set_a(prompt_char[2] == "1")
-        self.__set_a(prompt_char[3] == "1")
-        self.__set_a(prompt_char[4] == "1")
-        self.__set_a(prompt_char[5] == "1")
-        self.__set_a(prompt_char[6] == "1")
-        self.__toggle_display_on_or_off(prompt_char[7] == "1")
+        if prompt_char[7] == "1":
+            self.__set_a(prompt_char[0] == "1")
+            self.__set_b(prompt_char[1] == "1")
+            self.__set_c(prompt_char[2] == "1")
+            self.__set_d(prompt_char[3] == "1")
+            self.__set_e(prompt_char[4] == "1")
+            self.__set_f(prompt_char[5] == "1")
+            self.__set_g(prompt_char[6] == "1")
 
 
-    def __setA(self, state:bool):
+    def __set_a(self, state:bool) -> None:
         index = 0
         while index < 4:
             self.display[0][index] =  self.token if state else self.display[0][index]
+            index += 1
+
+    def __set_b(self, state:bool) -> None:
+        index = 0
+        while index < 3:
+            self.display[index][3] = self.token if state else self.display[index][3]
+            index += 1
+
+    def __set_c(self, state:bool) -> None:
+        index = 2
+        while index < 5:
+            self.display[index][3] = self.token if state else self.display[index][3]
+            index += 1
+
+    def __set_d(self, state:bool) -> None:
+        index = 3
+        while index >= 0:
+            self.display[4][index] = self.token if state else self.display[4][index]
+            index -= 1
+
+    def __set_e(self, state:bool) -> None:
+        index = 4
+        while index >= 2:
+            self.display[index][0] = self.token if state else self.display[index][0]
+            index -= 1
+
+    def __set_f(self, state:bool) -> None:
+        index = 2
+        while index >= 0:
+            self.display[index][0] = self.token if state else self.display[index][0]
+            index -= 1
+
+    def __set_g(self, state:bool) -> None:
+        index = 0
+        while index < 4:
+            self.display[2][index] = self.token if state else self.display[2][index]
+            index += 1
+
+    def __toggle_display_on_or_off(self, state:bool) -> None:
+        if not state: self.__set_display_blank()
+
+    def __set_display_blank(self):
+        index = 0
+        while index < len(self.display):
+            self.display[index] = [" ", " ", " ", " "]
+            index += 1
+
+    def get_display(self) -> [[]]:
+        return self.display
+
+    def __str__(self) -> str:
+        result = ""
+        for row in self.display:
+            for char in row:
+                result += char
+            result += "\n"
+        return result
+
+
 
--- assignment/feb_2025/seven_segment_display/test/seven_segment_display/test_seven_segment_display.py
+++ assignment/feb_2025/seven_segment_display/test/seven_segment_display/test_seven_segment_display.py
@@ -94,5 +94,68 @@ class TestSevenSegmentDisplay(unittest.TestCase):
     def setUp(self):
         self.display = SevenSegmentDisplay()
 
-    def test_something(self):
-        pass
+    def test_display_is_blank_when_blank_prompt_is_given(self):
+        self.display.enter_prompt(self.BLANK_PROMPT)
+        actual:[[]] = self.display.get_display()
+        print(self.display)
+        self.assertListEqual(self.BLANK, actual)
+
+    def test_display_is_one_when_one_prompt_is_given(self):
+        self.display.enter_prompt(self.ONE_PROMPT)
+        actual: [[]] = self.display.get_display()
+        print(self.display)
+        self.assertListEqual(self.ONE, actual)
+
+    def test_display_is_two_when_two_prompt_is_given(self):
+        self.display.enter_prompt(self.TWO_PROMPT)
+        actual: [[]] = self.display.get_display()
+        print(self.display)
+        self.assertListEqual(self.TWO, actual)
+
+    def test_display_is_three_when_three_prompt_is_given(self):
+        self.display.enter_prompt(self.THREE_PROMPT)
+        actual: [[]] = self.display.get_display()
+        print(self.display)
+        self.assertListEqual(self.THREE, actual)
+
+    def test_display_is_four_when_four_prompt_is_given(self):
+        self.display.enter_prompt(self.FOUR_PROMPT)
+        actual: [[]] = self.display.get_display()
+        print(self.display)
+        self.assertListEqual(self.FOUR, actual)
+
+    def test_display_is_five_when_five_prompt_is_given(self):
+        self.display.enter_prompt(self.FIVE_PROMPT)
+        actual: [[]] = self.display.get_display()
+        print(self.display)
+        self.assertListEqual(self.FIVE, actual)
+
+    def test_display_is_six_when_six_prompt_is_given(self):
+        self.display.enter_prompt(self.SIX_PROMPT)
+        actual: [[]] = self.display.get_display()
+        print(self.display)
+        self.assertListEqual(self.SIX, actual)
+
+    def test_display_is_seven_when_seven_prompt_is_given(self):
+        self.display.enter_prompt(self.SEVEN_PROMPT)
+        actual: [[]] = self.display.get_display()
+        print(self.display)
+        self.assertListEqual(self.SEVEN, actual)
+
+    def test_display_is_eight_when_eight_prompt_is_given(self):
+        self.display.enter_prompt(self.EIGHT_PROMPT)
+        actual: [[]] = self.display.get_display()
+        print(self.display)
+        self.assertListEqual(self.EIGHT, actual)
+
+    def test_display_is_nine_when_nine_prompt_is_given(self):
+        self.display.enter_prompt(self.NINE_PROMPT)
+        actual: [[]] = self.display.get_display()
+        print(self.display)
+        self.assertListEqual(self.NINE, actual)
+
+    def test_display_is_zero_when_zero_prompt_is_given(self):
+        self.display.enter_prompt(self.ZERO_PROMPT)
+        actual: [[]] = self.display.get_display()
+        print(self.display)
+        self.assertListEqual(self.ZERO, actual)
