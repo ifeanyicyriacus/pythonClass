1af9ff177eb16f06311f5bd1da3543ec9c2deaa0
ifeanyicyriacus <cyriacusifeanyi@gmail.com>
2025-03-02 11:54:07 +0100

    checkpoint

---
+++ assignment/feb_2025/tic_tac_toe/src/tictactoe/tic_tac_toe_game.py
@@ -0,0 +1,12 @@
+from src.tictactoe.token import Token
+from src.tictactoe.person import Person
+
+class TicTacToe:
+
+    def __init__(self, no_of_players:int, difficulty_level:int):
+        self.no_of_players = no_of_players
+        self.difficulty_level = difficulty_level
+        self.
+
+    def start_game(self):
+        pass
